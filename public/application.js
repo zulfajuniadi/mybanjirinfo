34c89a2b62c3bf3aadd5b4229080b446c607e0c7
Zulfa Juniadi Zulkifli <zulfajuniadi@gmail.com>
2014-12-27 04:55:44 +0800

    Check devtools is open

--- public/application.js
+++ public/application.js
@@ -277,3 +277,38 @@ var app = angular.module('mybanjir', ['ngRoute', 'ngDisqus', 'ui.bootstrap', 'jm
         });
         $rootScope.updateFeeds();
     });
+
+(function () {
+    var devtools = {open: false};
+    var threshold = 160;
+    var emitEvent = function (state) {
+        window.dispatchEvent(new CustomEvent('devtoolschange', {
+            detail: {
+                open: state
+            }
+        }));
+    };
+    setInterval(function () {
+        if ((window.Firebug && window.Firebug.chrome && window.Firebug.chrome.isInitialized) || window.outerWidth - window.innerWidth > threshold ||
+            window.outerHeight - window.innerHeight > threshold) {
+            if (!devtools.open) {
+                emitEvent(true);
+            }
+            devtools.open = true;
+        } else {
+            if (devtools.open) {
+                emitEvent(false);
+            }
+            devtools.open = false;
+        }
+    }, 500);
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = devtools;
+    } else {
+        window.devtools = devtools;
+    }
+})();
+
+window.addEventListener('devtoolschange', function (e) {
+    console.log('Care to help? Fork us on github: https://github.com/zulfajuniadi/mybanjirinfo');
+});
